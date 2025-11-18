11a7add66a8c6e74aba9f378e455498b6fd5b653
Uday Chauhan <chauhanuday4321@gmail.com>
2025-11-18 12:27:35 +0530

    first commit

---
+++ src/main/java/demo/docker_app/controller/Controller.java
@@ -0,0 +1,14 @@
+package demo.docker_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@GetMapping("/get")
+	public String getMessage() {
+		return "Hello Baby I love you.......";
+	}
+
+}
